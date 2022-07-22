bb36620f19a97836f16fa74c8d008d003e7797ff
Kiven Acevedo <kiven.acv2@gmail.com>
2022-07-22 12:13:17 -0400

    added unit testings of bad paths to uses cases tasks (usesCasesTask).

--- application/usescases/taskUsesCases_test.go
+++ application/usescases/taskUsesCases_test.go
@@ -1,7 +1,9 @@
 package usescases
 
 import (
+	"errors"
 	"github.com/BIGKaab/hexagonal-arquitecture-go/dummy"
+	"github.com/BIGKaab/hexagonal-arquitecture-go/infraestructure/inside/enum"
 	"github.com/BIGKaab/hexagonal-arquitecture-go/mocks"
 	"github.com/labstack/gommon/log"
 	"github.com/stretchr/testify/assert"
@@ -24,6 +26,18 @@ func TestInGetAllTasks_OK(t *testing.T) {
 	assert.Equal(t, dummy.TasksDomain, tasks)
 }
 
+func TestInGetAllTasks_Fail(t *testing.T) {
+	//mocks
+	repo := &mocks.TaskPortOut{}
+	repo.On("OutGetAllTasks").Return(nil, errors.New(enum.INTERNAL_SERVER_ERROR))
+	mapper := &mocks.TaskMapper{}
+	service := NewTaskPortOut(repo, mapper)
+	//assert
+	_, err := service.InGetAllTasks()
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New(enum.INTERNAL_SERVER_ERROR), err)
+}
+
 func TestInAddTask_OK(t *testing.T) {
 	//mocks
 	repo := &mocks.TaskPortOut{}
@@ -41,6 +55,19 @@ func TestInAddTask_OK(t *testing.T) {
 	assert.Equal(t, dummy.TaskDomain, task)
 }
 
+func TestInAddTask_Fail(t *testing.T) {
+	//mocks
+	repo := &mocks.TaskPortOut{}
+	repo.On("OutAddTask", &dummy.TaskEntity).Return(&dummy.TaskEntity, errors.New(enum.INTERNAL_SERVER_ERROR))
+	mapper := &mocks.TaskMapper{}
+	mapper.On("TaskDomainToEntity", dummy.TaskDomain).Return(dummy.TaskEntity)
+	service := NewTaskPortOut(repo, mapper)
+	//assert
+	_, err := service.InAddTask(dummy.TaskDomain)
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New(enum.INTERNAL_SERVER_ERROR), err)
+}
+
 func TestInFindTaskById_OK(t *testing.T) {
 	//mocks
 	repo := &mocks.TaskPortOut{}
@@ -55,6 +82,18 @@ func TestInFindTaskById_OK(t *testing.T) {
 	}
 	assert.NotNil(t, task)
 	assert.Equal(t, dummy.TaskDomain, task)
+}
+
+func TestInFindTaskById_Fail(t *testing.T) {
+	//mocks
+	repo := &mocks.TaskPortOut{}
+	repo.On("OutFindTaskById", 1).Return(dummy.TaskEntity, errors.New(enum.INTERNAL_SERVER_ERROR))
+	mapper := &mocks.TaskMapper{}
+	service := NewTaskPortOut(repo, mapper)
+	//assert
+	_, err := service.InFindTaskById(1)
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New(enum.INTERNAL_SERVER_ERROR), err)
 }
 
 func TestInUpdateTask_OK(t *testing.T) {
@@ -74,6 +113,19 @@ func TestInUpdateTask_OK(t *testing.T) {
 	assert.Equal(t, dummy.TaskDomain, task)
 }
 
+func TestInUpdateTask_Fail(t *testing.T) {
+	//mocks
+	repo := &mocks.TaskPortOut{}
+	repo.On("OutUpdateTask", 1, &dummy.TaskEntity).Return(&dummy.TaskEntity, errors.New(enum.INTERNAL_SERVER_ERROR))
+	mapper := &mocks.TaskMapper{}
+	mapper.On("TaskDomainToEntity", dummy.TaskDomain).Return(dummy.TaskEntity)
+	service := NewTaskPortOut(repo, mapper)
+	//assert
+	_, err := service.InUpdateTask(1, dummy.TaskDomain)
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New(enum.INTERNAL_SERVER_ERROR), err)
+}
+
 func TestDeleteTask_OK(t *testing.T) {
 	//mocks
 	repo := &mocks.TaskPortOut{}
@@ -86,4 +138,16 @@ func TestDeleteTask_OK(t *testing.T) {
 		log.Error(err)
 	}
 	assert.Nil(t, err)
+}
+
+func TestDeleteTask_Fail(t *testing.T) {
+	//mocks
+	repo := &mocks.TaskPortOut{}
+	repo.On("OutDeleteTask", 1).Return(errors.New(enum.INTERNAL_SERVER_ERROR))
+	mapper := &mocks.TaskMapper{}
+	service := NewTaskPortOut(repo, mapper)
+	//assert
+	err := service.InDeleteTask(1)
+	assert.NotNil(t, err)
+	assert.Equal(t, errors.New(enum.INTERNAL_SERVER_ERROR), err)
 }
--- infraestructure/inside/enum/errorEnum.go
+++ infraestructure/inside/enum/errorEnum.go
@@ -1,1 +1,7 @@
 package enum
+
+const (
+	INTERNAL_SERVER_ERROR = "Internal server error"
+	BAB_REQUEST           = "Bad request"
+	NOT_FOUND             = "Not found"
+)
